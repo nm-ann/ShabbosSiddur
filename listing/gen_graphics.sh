3d315e5d5fd7d661e7c1b0b36e00f15760d1ed8b
Noam Annenberg <noam.annenberg@gmail.com>
2022-03-08 20:00:40 -0500

    fix issue with small iphone sizing

--- listing/gen_graphics.sh
+++ listing/gen_graphics.sh
@@ -1,104 +1,123 @@
-# !/bin/bash
-# Usage: ./gen_graphics.sh -c background_color -i icon_img_path
-while getopts "i:c:" OPTION
-do
-    case $OPTION in
-    c)
-        COLOR=$OPTARG
-        ;;
-    i)
-        IMG=$OPTARG
-        ;;
-    esac
-done
-if [ -z $IMG ]; then
-    echo "Usage: ./gen_graphics.sh -c background_color -i icon_img_path"
-    exit 1
-fi
-if [ -z $COLOR ]; then
-    COLOR="\#deb744"
-fi
-
-# echo "CREATING BACKGROUND IMAGES"
-convert -size 1080x2160 xc:$COLOR screenshots/background_android.png 
-convert -size 1284x2778 xc:$COLOR screenshots/background_iphone_big.png 
-convert -size 1242x2208 xc:$COLOR screenshots/background_iphone_small.png
-convert -size 2048x2732 xc:$COLOR screenshots/background_ipad.png
-
-echo "Generating 5.5 inch iPhone screenshots"
-for file in screenshots/ios/*_iphone.png;
-do ext="${file##*.}"; filename="${file%.*}";
-cp "$file" "${filename}_small.${ext}";
-mv "$file" "${filename}_big.${ext}";
-convert "${filename}_small.${ext}" -resize 1242x2208 "${filename}_small.${ext}"
-convert "${filename}_big.${ext}" -resize 1284x2778 "${filename}_big.${ext}"
-
-done
-
-echo "CREATING SCREENSHOTS"
-cd screenshots
-fastlane frameit
-cd ..
-
-echo "MAKING ANDROID ICONS"
-mkdir icons
-mkdir icons/android
-mkdir icons/ios
-mkdir icons/android/mipmap-xxxhdpi/
-mkdir icons/android/mipmap-xxhdpi/
-mkdir icons/android/mipmap-xhdpi/
-mkdir icons/android/mipmap-hdpi/
-mkdir icons/android/mipmap-mdpi/
-mkdir icons/android/mipmap-ldpi/
-
-imgName="${IMG%.*}";
-convert -size 192x192 xc:Black -fill White -draw 'circle 96 96 96 1' -alpha Copy mask192.png
-convert $IMG -resize 192x192 icons/android/mipmap-xxxhdpi/ic_launcher.png
-convert icons/android/mipmap-xxxhdpi/ic_launcher.png -gravity Center mask192.png -compose CopyOpacity -composite -trim icons/android/mipmap-xxxhdpi/ic_launcher_round.png
-convert -size 144x144 xc:Black -fill White -draw 'circle 72 72 72 1' -alpha Copy mask144.png
-convert $IMG -resize 144x144 icons/android/mipmap-xxhdpi/ic_launcher.png
-convert icons/android/mipmap-xxhdpi/ic_launcher.png -gravity Center mask144.png -compose CopyOpacity -composite -trim icons/android/mipmap-xxhdpi/ic_launcher_round.png
-convert -size 96x96 xc:Black -fill White -draw 'circle 48 48 48 1' -alpha Copy mask96.png
-convert $IMG -resize 96x96 icons/android/mipmap-xhdpi/ic_launcher.png
-convert icons/android/mipmap-xhdpi/ic_launcher.png -gravity Center mask96.png -compose CopyOpacity -composite -trim icons/android/mipmap-xhdpi/ic_launcher_round.png
-convert -size 72x72 xc:Black -fill White -draw 'circle 36 36 36 1' -alpha Copy mask72.png
-convert $IMG -resize 72x72 icons/android/mipmap-hdpi/ic_launcher.png
-convert icons/android/mipmap-hdpi/ic_launcher.png -gravity Center mask72.png -compose CopyOpacity -composite -trim icons/android/mipmap-hdpi/ic_launcher_round.png
-convert -size 48x48 xc:Black -fill White -draw 'circle 24 24 24 1' -alpha Copy mask48.png
-convert $IMG -resize 48x48 icons/android/mipmap-mdpi/ic_launcher.png
-convert icons/android/mipmap-mdpi/ic_launcher.png -gravity Center mask48.png -compose CopyOpacity -composite -trim icons/android/mipmap-mdpi/ic_launcher_round.png
-convert -size 36x36 xc:Black -fill White -draw 'circle 18 18 18 1' -alpha Copy mask36.png
-convert $IMG -resize 36x36 icons/android/mipmap-ldpi/ic_launcher.png
-convert icons/android/mipmap-ldpi/ic_launcher.png -gravity Center mask36.png -compose CopyOpacity -composite -trim icons/android/mipmap-ldpi/ic_launcher_round.png
-
-cp -r icons/android/* ../android/app/src/main/res/
-
-echo "MAKING IOS ICONS"
-convert $IMG -resize 20x20! icons/ios/$imgName-20.png
-convert $IMG -resize 22x22! icons/ios/$imgName-22.png
-convert $IMG -resize 29x29! icons/ios/$imgName-29.png
-convert $IMG -resize 40x40! icons/ios/$imgName-40.png
-convert $IMG -resize 44x44! icons/ios/$imgName-44.png
-convert $IMG -resize 48x48! icons/ios/$imgName-48.png
-convert $IMG -resize 55x55! icons/ios/$imgName-55.png
-convert $IMG -resize 58x58! icons/ios/$imgName-58.png
-convert $IMG -resize 60x60! icons/ios/$imgName-60.png
-convert $IMG -resize 66x66! icons/ios/$imgName-66.png
-convert $IMG -resize 76x76! icons/ios/$imgName-76.png
-convert $IMG -resize 80x80! icons/ios/$imgName-80.png
-convert $IMG -resize 87x87! icons/ios/$imgName-87.png
-convert $IMG -resize 88x88! icons/ios/$imgName-88.png
-convert $IMG -resize 120x120! icons/ios/$imgName-120.png
-convert $IMG -resize 152x152! icons/ios/$imgName-152.png
-convert $IMG -resize 167x167! icons/ios/$imgName-167.png
-convert $IMG -resize 172x172! icons/ios/$imgName-172.png
-convert $IMG -resize 180x180! icons/ios/$imgName-180.png
-convert $IMG -resize 196x196! icons/ios/$imgName-196.png
-convert $IMG -resize 1024x1024! icons/ios/$imgName-1024.png
-
-convert $IMG -resize 512x512! $imgName-512.png
-
-COLOR_GREEN='\033[0;32m'
-COLOR_DEFAULT='\033[0m'
-echo -e "${COLOR_GREEN}FINISHED${COLOR_DEFAULT}"
-exit 0
+{
+    "maariv": {
+        "title": "Maariv",
+        "firstChapter": 0,
+        "chapters": [
+            {
+                "collection": "maariv",
+                "title": "Maariv",
+                "chapterNum": 1
+            }
+        ]
+    },
+    "shach": {
+        "title": "Shacharit",
+        "chapters": [
+            {
+                "collection": "shach",
+                "title": "Shochen Ad",
+                "chapterNum": 1
+            },
+            {
+                "collection": "shach",
+                "title": "Yishtabach",
+                "chapterNum": 2
+            },
+            {
+                "collection": "shach",
+                "title": "Birchot Kriat Shema",
+                "chapterNum": 3
+            },
+            {
+                "collection": "shach",
+                "title": "Avot",
+                "chapterNum": 4
+            },
+            {
+                "collection": "shach",
+                "title": "Kedushat Hayom",
+                "chapterNum": 5
+            },
+            {
+                "collection": "shach",
+                "title": "Hodaah",
+                "chapterNum": 6
+            },
+            {
+                "collection": "supp",
+                "title": "Shabbat Additions",
+                "chapterNum": 1
+            },
+            {
+                "collection": "shach",
+                "title": "Hallel",
+                "chapterNum": 7
+            },
+            {
+                "collection": "shach",
+                "title": "Hotzaat Sefer Torah",
+                "chapterNum": 8
+            }
+        ]
+    },
+    "musaf": {
+        "title": "Mussaf",
+        "chapters": [
+            {
+                "collection": "musaf",
+                "title": "Ashrei",
+                "chapterNum": 1
+            },
+            {
+                "collection": "musaf",
+                "title": "Avot",
+                "chapterNum": 2
+            },
+            {
+                "collection": "musaf",
+                "title": "Kedushat Hayom",
+                "chapterNum": 3
+            },
+            {
+                "collection": "musaf",
+                "title": "Hodaah",
+                "chapterNum": 4
+            },
+            {
+                "collection": "supp",
+                "title": "Shabbat Additions",
+                "chapterNum": 2
+            },
+            {
+                "collection": "supp",
+                "title": "Korban Hayom",
+                "chapterNum": 3
+            }
+        ]
+    },
+    "mincha": {
+        "title": "Mincha",
+        "chapters": [
+            {
+                "collection": "mincha",
+                "title": "Ashrei, Avot",
+                "chapterNum": 1
+            },
+            {
+                "collection": "mincha",
+                "title": "Kedushat Hayom",
+                "chapterNum": 2
+            },
+            {
+                "collection": "mincha",
+                "title": "Hodaah",
+                "chapterNum": 3
+            },
+            {
+                "collection": "mincha",
+                "title": "Sim Shalom",
+                "chapterNum": 4
+            }
+        ]
+    }
+}
